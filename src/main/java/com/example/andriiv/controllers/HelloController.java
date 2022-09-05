444e51586668fe55e00301f8b042ebf48498635f
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-05 14:41:12 +0200

    77. First Spring Security application

---
+++ src/main/java/com/example/andriiv/controllers/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.andriiv.controllers;
+
+import com.example.andriiv.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Roman_Andriiv
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+}
