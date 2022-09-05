e325eb3b92fa010713351ccf1412b0f004c58669
Roman Andriiv <roman.andriyiv@gmail.com>
2022-09-05 14:58:13 +0200

    78. Authentication II in Spring Security (UserDetailsService)

--- src/main/java/com/example/andriiv/config/SecurityConfig.java
+++ src/main/java/com/example/andriiv/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.andriiv.config;
 
-import com.example.andriiv.security.AuthProviderImpl;
+import com.example.andriiv.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Roman_Andriiv
@@ -12,15 +15,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider); // TODO
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
 
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
